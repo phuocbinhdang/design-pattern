4475fa3758a1d3c6930e2cf0976b21f9433f1c89
mothan <mothan@nexondv.com>
2024-04-01 16:26:37 +0700

    feat: stategy design pattern

---
+++ behavioral/stategy.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+
+
+class Notifier(ABC):
+    @abstractmethod
+    def send(message: str) -> None:
+        pass
+
+
+class EmailNotifier(Notifier):
+    _email: str
+
+    def __init__(self, email) -> None:
+        self._email = email
+
+    def send(self, message: str) -> None:
+        print("Sending Notification")
+        print(f"Email: {self._email}")
+        print(f"{message}")
+
+
+class SMSNotifier(Notifier):
+    _phone_number: str
+
+    def __init__(self, phone_number) -> None:
+        self._phone_number = phone_number
+
+    def send(self, message: str) -> None:
+        print("Sending Notification")
+        print(f"Phone number: {self._phone_number}")
+        print(f"{message}")
+
+
+class BanAccountService:
+    _notifier: Notifier
+
+    def __init__(self, notifier) -> None:
+        self._notifier = notifier
+
+    def execute(self, reason) -> None:
+        message: str = f"Message: You are baned\nReason: {reason}"
+        self._notifier.send(message)
+
+
+if __name__ == "__main__":
+    service: BanAccountService = BanAccountService(
+        notifier=EmailNotifier(email="you@example.com")
+    )
+
+    service.execute("Used 3rd party software")
+
+    service: BanAccountService = BanAccountService(
+        notifier=SMSNotifier(phone_number="0123456789")
+    )
+
+    service.execute("Used 3rd party software")
